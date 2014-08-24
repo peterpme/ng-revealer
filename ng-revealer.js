7436fd85c8b7a972099a07ed7525657137e774e7
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2014-08-24 15:22:58 -0500

    restructured minute changes

--- ng-revealer.js
+++ ng-revealer.js
@@ -1,63 +1,65 @@
-conyers.ng.directive('revealer', ['$log', function ($log) {
+(function () {
+    angular.module('ng-reaveler', [])
+    .directive('ng-reavealer', ['$log', function ($log) {
 
-    var active = false,
-    activeClsName = 'active';
+        var active = false,
+        activeClsName = 'revealed';
 
-    function Revealer (btn, hiddenText) {
-        this.btn = btn;
-        this.hiddenText = hiddenText;
+        function Revealer (btn, hiddenText) {
+            this.btn = btn;
+            this.hiddenText = hiddenText;
 
-        this.activateState();
-    }
-
-    Revealer.prototype = {
-
-        show: function () {
-            this.hiddenText.addClass(activeClsName);
-            this.btn.addClass(activeClsName);
-        },
-
-        hide: function () {
-            this.hiddenText.removeClass(activeClsName);
-            this.btn.removeClass(activeClsName);
-        },
-
-        activateState: function () {
-            var _this = this;
-
-            this.btn.on('click', function (e) {
-                e.preventDefault();
-                if (active === false) {
-                    _this.show();
-                    _this.replaceActiveHtml();
-                } else {
-                    _this.hide();
-                    _this.removeActiveHtml();
-                }
-                active = !active;
-            });
-        },
-
-        replaceActiveHtml: function() {
-            this.btn.html('<i class="fa fa-minus"></i> Read Less');
-        },
-
-        removeActiveHtml: function() {
-            this.btn.html('<i class="fa fa-plus"></i> Read More');
+            this.activateState();
         }
 
-    }
+        Revealer.prototype = {
 
-    return {
-        restrict: 'A',
-        link: function (scope, element) {
-            $log.info('[readMore] init');
+            show: function () {
+                this.hiddenText.addClass(activeClsName);
+                this.btn.addClass(activeClsName);
+            },
 
-            var hidden = angular.element('.inactive', element),
-            btn = angular.element('.btn-readMore');
+            hide: function () {
+                this.hiddenText.removeClass(activeClsName);
+                this.btn.removeClass(activeClsName);
+            },
 
-            new Revealer(btn, hidden);
+            activateState: function () {
+                var _this = this;
+
+                this.btn.on('click', function (e) {
+                    e.preventDefault();
+                    if (active === false) {
+                        _this.replaceActiveHtml();
+                        _this.show();
+                    } else {
+                        _this.removeActiveHtml();
+                        _this.hide();
+                    }
+                    active = !active;
+                });
+            },
+
+            replaceActiveHtml: function() {
+                this.btn.html('<i class="fa fa-minus"></i> Reveal Less');
+            },
+
+            removeActiveHtml: function() {
+                this.btn.html('<i class="fa fa-plus"></i> Reveal More');
+            }
+
         }
-    }
 
-}]);
+        return {
+            restrict: 'A',
+            link: function (scope, element) {
+                $log.info('[reveal] init');
+
+                var hidden = angular.element('.hidden-content', element),
+                btn = angular.element('.btn-reveal');
+
+                new Revealer(btn, hidden);
+            }
+        }
+    }]);
+}());
